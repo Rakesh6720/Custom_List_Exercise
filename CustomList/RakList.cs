31475d6c6c99ebf35f8afe9e7f314ca3eca7fb38
Rakesh <rbaruah@gmail.com>
2018-05-11 16:37:49 -0500

    finished the test methods for add and remove.  wrote the add method.  now going to begin writing the remove method.

--- CustomList/RakList.cs
+++ CustomList/RakList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,29 +7,88 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class RakList<T>
+    public class RakList<T> : IEnumerable<T>
     {
-        T [] data;
+        T [] array;
         int count;
         int capacity;
 
         public RakList()
         {
             count = 0;
-            capacity = 5;
-            data = new T [capacity];
-            
+            capacity += 5;
+            array = new T [capacity];
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+            set
+            {
+                array[i] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // how to enumerate
+            return (IEnumerator)GetEnumerator();
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            // how to enumerator
+        }
+
+        public bool CheckCapacity(int count, int capacity)
+        {
+            if (count > capacity)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public void Add(T value)
         {
-            
+            bool trueFalse = CheckCapacity(count, capacity);
+            if (trueFalse == true)
+            {
+                array[count] = value;
+            }
+            else
+            {
+                capacity = capacity * 2;
+                T [] tempArray = new T [capacity];
+
+               for (int i = 0; i<=count; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+
+                array = tempArray;
+                
+            }
             
         }
 
         public void Remove(T value)
         {
-            
+
         }
     }
 }
--- CustomList_Test/RakList_Test.cs
+++ CustomList_Test/RakList_Test.cs
@@ -14,7 +14,6 @@ namespace CustomList_Test
         {
             //Arrange
             RakList<int> list = new RakList<int>();
-            
             int expectedResult = 5;
             
             //Act
@@ -32,62 +31,61 @@ namespace CustomList_Test
             RakList<string> list = new RakList<string>() { "alpha", "bravo", "charlie", "delta"};
 
             string input = "echo";
-            string actualResult = input;
             
             //Act
             list.Add(input);
-            string expectedResult = list[4];
+            string expectedResult = input;
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(input, list[4]);
         }
 
         [TestMethod]
         
-        public void Add_AddItemToFullList()
+        public void Add_AddItemToFullList_AddToNewIndex()
         {
             //Arrange
             RakList<int> list = new RakList<int>() { 0, 1, 2, 3, 4 };
-            int input = 5;
+            int newInput = 5;
+            int actualResult = newInput;
+
             //Act
-            list.Add(input);
+            list.Add(newInput);
 
             //Assert
-            Assert.ThrowsException(IndexOutOfRangeException);//how will count and capacity be affected?  this should not be an exception.
+            Assert.AreEqual(newInput, list[6]); //input will be found at newList[0];
+        }
+
+        public void Add_AddItemToFullList_DoubleCount()
+        {
+            //Arrange
+            RakList<int> list = new RakList<int>() { 0, 1, 2, 3, 4 };
+            int newInput = 5;
+            
+            //Act
+            list.Add(newInput);
+
+            //Assert
+            Assert.AreEqual(list.Count, 10); 
         }
 
         [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
         public void Remove_ItemFromEmptyList()
         {
             //Arrange
             RakList<string> list = new RakList<string>();
-            string expectedValue = "referenceException";
-            
-
 
             //Act
-            list.Remove(actualValue);
+            list.Remove("value");
             string actualValue = list[0];
 
             //Assert
-            Assert.AreEqual(expectedValue, actualValue); // assert value throws error
+            
         }
 
         [TestMethod]
-        public void Remove_NonExistingItem(int num) //this is repetitive from the one above
-        {
-            //Arrange
-            RakList<int> list = new RakList<int>() { 0, 1, 2, 3, 4 };
-            int actualValue = 6;
-            //Act
-            list.Remove(actualValue);
-
-            //Assert
-            Assert.ThrowsException(IndexOutOfRangeException);
-        }
-
-        [TestMethod]
-        public void Remove_MoreThanOneItemExists(string value)
+        public void Remove_MoreThanOneItemExists_ChecksCount()
         {
             //Arrange
             RakList<string> list = new RakList<string>() { "alpha", "bravo", "charlie", "delta", "echo", "alpha" };
@@ -95,11 +93,110 @@ namespace CustomList_Test
             
 
             //Act
-            list.Remove(value);
+            list.Remove("alpha");
             string actualValue = list[4];
-
+            
             //Assert
             Assert.AreEqual(expectedValue, actualValue);
+            int newCapacity = list.Count - 1;
+
+            //test that count goes down
+            Assert.AreEqual(newCapacity, list.Count);
+        }
+
+        [TestMethod]
+        public void Remove_NoItemExists()
+        {
+            //Arrange
+            RakList<int> list = new RakList<int>() { 0, 1, 2, 3, 4 };
+            int input = 5;
+
+            //Act
+            list.Remove(input);
+            int actualValue = list.Count;
+            int expectedValue = 5;
+
+            //Assert
+            //remove something that doesn't exist count remains same
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void Remove_NoItemExists_IndexRemainsSame()
+        {
+            //Arrange
+            RakList<int> list = new RakList<int> { 0, 1, 2, 3, 4 };
+            int input = 5;
+
+            //Act
+            list.Remove(input);
+
+            //Asser2
+            Assert.AreEqual(0, list[3]);
+        }
+
+        [TestMethod]
+        
+        public void Remove_NoItemExists_ReturnFalse()
+        { 
+            //Arrange
+            RakList<int> list = new RakList<int> { 0, 1, 2, 3, 4 };
+            int input = 5;
+            bool foundItem;
+
+            //Act
+            list.Remove(input);
+            foundItem = false;
+
+            //Assert
+            Assert.IsFalse(foundItem);
+        }
+
+        [TestMethod]
+        public void Remove_ItemExists_ReturnTrue()
+        {
+            //Arrange
+            RakList<int> list = new RakList<int> { 0, 1, 2, 3, 4 };
+            int input = 4;
+            bool foundItem;
+
+            //Act
+            list.Remove(input);
+            foundItem = true;
+
+            //Assert
+            Assert.IsTrue(foundItem);
+        }
+
+        [TestMethod]
+        public void Remove_ItemExists_CheckIndex()
+        {
+            //Arrange
+            RakList<int> list = new RakList<int> { 0, 1, 2, 3, 4 };
+            int input = 2;
+            
+            //Act
+            list.Remove(input);
+            int expectedValue = 4;
+
+            //Assert
+            Assert.AreEqual(expectedValue, list[3]);
+        }
+
+        [TestMethod] 
+        public void Remove_ItemExists_CheckCount()
+        {
+            //Arrange
+            RakList<int> list = new RakList<int>() { 0, 1, 2, 3, 4 };
+            int input = 2;
+
+            //Act
+            list.Remove(input);
+            int expectedValue = list.Count - 1;
+
+
+            //Assert
+            Assert.AreEqual(expectedValue, list.Count);
         }
     }
 }
